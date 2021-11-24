aa779b75eaf17f36ddf06cc2e4a688c2be63a20c
Jonathanrange <jonathan.range@outlook.com>
2021-11-24 22:00:12 +0000

    Add function get_input_data to get data from user

--- run.py
+++ run.py
@@ -14,4 +14,18 @@ SHEET = GSPREAD_CLIENT.open('general_taste_survey')
 
 music = SHEET.worksheet('music')
 data = music.get_all_values()
-print(data)
+
+
+def get_input_data():
+    '''
+    Get input data from user
+    '''
+    print('Welcome to the General Taste Survey!')
+    print('------------------------------------\n')
+    print('This survey contains answers from 150 people')
+    print('The categories are: Music, Movies and Sports\n')
+    data_str = input('Please enter a category here: ')
+    print(f'You have selected: {data_str}')
+
+
+get_input_data()
